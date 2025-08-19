b8e0a8b5d104ac5b99d3cd42087cad4a6c7cfece
Harsh Bhamare <harshbhamaremit@gmail.com>
2025-08-19 11:44:34 +0530

    minor changes in index.js

--- api/index.js
+++ api/index.js
@@ -24,10 +24,12 @@ mongoose.connect(dbURI, {
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(cookieParser());
 
 
